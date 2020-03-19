958e76b1286c468b05a9613b765c6c8844cd1468
dessafri <safrides@gmail.com>
2020-03-19 17:23:04 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home
+    },
+    {
+        path: '/about',
+        name: 'About',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    },
+    {
+        path: '/Product',
+        name: 'Product',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Product.vue')
+    },
+    {
+        path: '/Cart',
+        name: 'Cart',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Cart.vue')
+    },
+    {
+        path: '/Success',
+        name: 'Success',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/Success.vue')
+    }
+]
+
+const router = new VueRouter({
+    routes
+})
+
+export default router
---
+++ src/views/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <HeaderShayna />
+    <HeroShayna />
+    <BannerShayna />
+    <GalleryShayna />
+    <PartnersShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderShayna from "@/components/HeaderShayna.vue";
+import HeroShayna from "@/components/HeroShayna.vue";
+import BannerShayna from "@/components/BannerShayna.vue";
+import GalleryShayna from "@/components/GalleryShayna.vue";
+import PartnersShayna from "@/components/PartnersShayna.vue";
+import FooterShayna from "@/components/FooterShayna.vue";
+
+export default {
+  name: "Home",
+  components: {
+    // HelloWorld
+    HeaderShayna,
+    HeroShayna,
+    BannerShayna,
+    GalleryShayna,
+    PartnersShayna,
+    FooterShayna
+  }
+};
+</script>
